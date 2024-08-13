addbaa831cebc3940e50b6c7eaffa5397a5ed293
RohitSathya <156504207+RohitSathya@users.noreply.github.com>
2024-08-13 00:52:41 -0700

    Update App.jsx

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -369,7 +369,7 @@ function AppContent() {
             path="/"
             element={
               <>
-             <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8">
+           <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-4 xl:grid-cols-6 gap-8">
   {filteredProducts.map((p) => (
     <Products key={p.name} data={p} func={getcount} namefunc={setUsername} pi={setProductinfo} />
   ))}
